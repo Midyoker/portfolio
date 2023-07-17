99c1ce0503b45275a56a71c4c5b69f2b97814751
Mohit <mohitsharma20020720@gmail.com>
2023-07-17 17:35:46 +0530

    Personal Portfolio

---
+++ components/Header.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons';
+import { motion } from 'framer-motion';
+import { GetStaticPaths } from 'next'
+
+type Props = {}
+
+export default function header({ }: Props) {
+    return ( 
+        <header className='sticky flex-0 flex item-start justify-between max-w-7xl mx-auto z-20 xl:items-center p-5'>
+            <motion.div
+                initial={{ 
+                    opacity: 0, 
+                    x:-500,
+                    scale: 0.5
+                }} 
+                animate={{
+                    opacity: 1,
+                    x:0,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5,
+                    delay: 0.3
+                }}
+            className='flex flex-row items-center '>
+                <SocialIcon url="https://www.linkedin.com/mohit-sharma-643508227/"
+                    fgColor="gray"
+                    bgColor="transparent" />
+                <SocialIcon url="https://www.linkedin.com/in/alexander-lee-1b1b1b1b1/"
+                    fgColor="gray"
+                    bgColor="transparent" />
+                <SocialIcon url="https://www.linkedin.com/in/alexander-lee-1b1b1b1b1/"
+                    fgColor="gray"
+                    bgColor="transparent" />
+                <SocialIcon url="https://www.linkedin.com/in/alexander-lee-1b1b1b1b1/"
+                    fgColor="gray"
+                    bgColor="transparent" />
+
+            </motion.div>
+            <motion.div
+              initial={{ 
+                opacity: 0, 
+                x:500,
+                scale: 0.5
+            }} 
+            animate={{
+                opacity: 1,
+                x:0,
+                scale: 1
+            }}
+            transition={{
+                duration: 1.5,
+                delay: 0.3
+            }}
+             className='flex flex-row items-center text-gray-400 cursor-pointer'>
+                <SocialIcon 
+                className='cursor-pointer'
+                network='email'
+                fgColor="gray"
+                bgColor="transparent">
+
+                </SocialIcon>
+                <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>  Get in Touch</p>
+            </motion.div>
+         
+        </header>
+    )
+}
